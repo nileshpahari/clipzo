a7fbdfcbbb013de60f4fd11140d48318632af1cf
Nilesh Pahari <nileshpahari@protonmail.com>
2025-07-02 04:06:01 +0530

    add credits boilerplate

---
+++ src/app/credits/page.tsx
@@ -0,0 +1,9 @@
+export default function Thanks() {
+    return (
+        <div className="h-screen w-full flex flex-col items-center justify-center gap-4 text-2xl">
+            <div>yt-dlp: for downloading videos</div>
+            <div>clippa.in: project inspiration</div>
+            <div>ytdlp.online: feature inspiration</div>
+        </div>
+    );
+}
