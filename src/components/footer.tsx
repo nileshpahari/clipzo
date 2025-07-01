ae6023f0540108a5f801960fddb6f6d3707f4246
Nilesh Pahari <nileshpahari@protonmail.com>
2025-07-02 02:18:19 +0530

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,25 +1,26 @@
 import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="text-muted-foreground flex items-center justify-center gap-2 p-4">
+    <footer className="text-muted-foreground flex flex-col sm:flex-row items-center justify-center gap-2 sm:gap-4 p-4 text-sm text-center">
       <p>© 2025 Clipzo. All rights reserved.</p>
-      <div>
-        {" "}
-        <Link
-          href="https://github.com/nileshpahari/clipzo"
-          target="_blank"
-          className="underline"
-        >
-          Repo
-        </Link>
-        <span>.</span>
-      </div>
-      <div>
-        {" "}
-        <Link href="/thankful" className="underline">
-          Credits
-        </Link>
-        <span>.</span>
+      <div className="flex gap-2">
+        <div>
+          <Link
+            href="https://github.com/nileshpahari/clipzo"
+            target="_blank"
+            className="underline"
+          >
+            Repo
+          </Link>
+          <span className="hidden sm:inline">.</span>
+        </div>
+        <div>
+          <Link href="/thankful" className="underline">
+            Credits
+          </Link>
+          <span className="hidden sm:inline">.</span>
+        </div>
       </div>
     </footer>
   );
